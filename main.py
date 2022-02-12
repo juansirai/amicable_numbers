ed7df056ee973af0d073ebd879f5fddc6a6ddc6c
Juan B. Sirai <juanbsirai664@gmail.com>
2022-02-12 20:11:49 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+def d(n:int) -> int:
+  # makes the sum of proper divisors of n (numbers less than n which divide evenly into n)
+    top = int(n/2) # since the biggest integer divisor of a number is its half.
+    sum = 0
+    for i in range(1,top+1):
+        if n%i == 0: 
+            # evaluates if the rest of the division is zero
+            sum +=i
+    return sum
+
+def sum_amicables(max):
+    sum = 0
+    for n1 in range(2, max+1):
+        # includes the max in the for loop
+        n2 = d(d(n1))
+        # Optional line to debug the code:
+        # print(f"n1: {n1}, d1:{d(n1)} and d(d1): {n2}, Amicable? {n1==n2}")
+        if (n1 == n2):
+            if (n2<=max):
+              # Performs the sum only if n2 is less or equal than the max (in our case 10.000)
+              # Only checks for n2, because size of n1 is controled by the for loop
+                sum = sum + n1 + n2
+    return sum
+
+print(sum_amicables(10000))

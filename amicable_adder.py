8e5265c994adce59ffe05a54cd9ebc0f0b3522d8
Juan B. Sirai <juanbsirai664@gmail.com>
2022-02-13 10:14:42 -0300

    Update amicable_adder.py

--- amicable_adder.py
+++ amicable_adder.py
@@ -13,17 +13,17 @@ def sum_amicables(max):
     return result
   
   # here starts the code to perform the sum of all amicable numbers lower than a paramm
-    sum = 0
-    for n1 in range(2, max+1):
-        # includes the max in the for loop
-        n2 = d(d(n1))
-        # Optional line to debug the code:
-        # print(f"n1: {n1}, d1:{d(n1)} and d(d1): {n2}, Amicable? {n1==n2}")
-        if (n1 == n2):
-            if (n2<=max):
-              # Performs the sum only if n2 is less or equal than the max (in our case 10.000)
-              # Only checks for n2, because size of n1 is controled by the for loop
-                sum = sum + n1 + n2
-    return sum
+  sum = 0
+  for n1 in range(2, max+1):
+    # includes the max in the for loop
+    n2 = d(d(n1))
+    # Optional line to debug the code:
+    # print(f"n1: {n1}, d1:{d(n1)} and d(d1): {n2}, Amicable? {n1==n2}")
+    if (n1 == n2):
+      if (n2<=max):
+      # Performs the sum only if n2 is less or equal than the max (in our case 10.000)
+      # Only checks for n2, because size of n1 is controled by the for loop
+        sum = sum + n1 + n2
+  return sum
 
 
